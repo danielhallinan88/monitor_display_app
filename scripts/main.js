2d3a4499969b5b9ee9df25a5356e372bae05bceb
danielhallinan88 <danielhallinan88@gmail.com>
2019-10-13 12:10:20 -0500

    time reformat, file structuring

---
+++ scripts/main.js
@@ -0,0 +1,34 @@
+var span = document.getElementById('clock');
+
+function add_zero_str(num) {
+  if (num < 10) {
+    num = "0" + num;
+  }
+  return num;
+}
+
+function time() {
+  var d = new Date();
+  var s = add_zero_str(d.getSeconds());
+  var m = add_zero_str(d.getMinutes());
+  var h = d.getHours();
+  span.textContent = h + ":" + m + ":" + s;
+}
+
+setInterval(time, 1000);
+
+
+var elem = document.documentElement;
+function openFullscreen() {
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.mozRequestFullScreen) { /* Firefox */
+    elem.mozRequestFullScreen();
+  } else if (elem.webkitRequestFullscreen) { /* Chrome, Safari & Opera */
+    elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) { /* IE/Edge */
+    elem.msRequestFullscreen();
+  }
+
+  document.getElementById("fullScreenButton").style.display="none";
+}
